a687beb6a957f5901cf3f0112871aaeaa2170cfa
Виктория Аверьянова <viktorijajazva@gmail.com>
2023-04-24 09:22:00 +0300

    240423

--- WpfApp3/MyPage/GroupPage.xaml.cs
+++ WpfApp3/MyPage/GroupPage.xaml.cs
@@ -12,30 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfApp3.MyPage;
-using WpfApp3.Componens;
-
 
 namespace WpfApp3.MyPage
 {
     /// <summary>
-    /// Логика взаимодействия для HomePage.xaml
+    /// Логика взаимодействия для GroupPage.xaml
     /// </summary>
-    public partial class HomePage : Page
+    public partial class GroupPage : Page
     {
-        public HomePage()
+        public GroupPage()
         {
             InitializeComponent();
-        }
-
-        private void Li4noeBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Navigation.NextPage(new Nav("", new PersonalPage()));
-        }
-
-        private void GroupBtn_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new GroupPage());
         }
     }
 }
